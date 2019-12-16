c0d62926e0e174c8954f18da4b48a05032298774
Carolina Arvidsson <carolinaarvidsson_@live.se>
2019-12-17 00:15:29 +0100

    JS elements done

---
+++ Index.js
@@ -0,0 +1,59 @@
+var i; // Loopvariable
+
+function init() {
+    console.log('dwdawdaw');
+    initUI();
+}
+
+function initUI() {
+    // header
+    let header = document.createElement('header');
+    header.id = 'header';
+    document.body.appendChild(header);
+
+    let headerH1 = document.createElement('h1');
+    headerH1.innerText = 'Calendar application 2000';
+    header.appendChild(headerH1);
+
+    // wrapper
+    let divWrapper = document.createElement('div');
+    divWrapper.id = 'wrapper';
+    document.body.appendChild(divWrapper);
+
+    // sidebar
+    let divSidebar = document.createElement('div');
+    divSidebar.id = 'sidebar';
+    divWrapper.appendChild(divSidebar);
+
+    let nav = document.createElement('nav');
+    divSidebar.appendChild(nav);
+
+    let ul = document.createElement('ul');
+    nav.appendChild(ul);
+
+    let itemList = ['New activity', 'Calendar'];
+
+    for(i = 0; i < itemList.length; i++) {
+        console.log(itemList[i]);
+        let item = document.createElement('li');
+        item.innerText = itemList[i];
+        ul.appendChild(item);
+    }
+    
+
+ /*   let li1 = document.createElement('li');
+    li1.innerText = ('hej');
+    ul.appendChild(li1);*/
+    
+
+    // main
+    let main = document.createElement('main');
+    main.id = 'main-container';
+    divWrapper.appendChild(main);
+
+    let mainh2 = document.createElement('h2');
+    mainh2.innerText = 'Calendar';
+    main.appendChild(mainh2);
+}
+
+window.onload = init; // Make sure init is enabled when the page is loaded.
